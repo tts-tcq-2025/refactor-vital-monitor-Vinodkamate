127a7c33549f8216212600d2fa4dcc41f82b54f7
Vinodkamate <vinodkamate4@gmail.com>
2025-08-13 19:46:58 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,11 +2,12 @@
 #include "./monitor.h"
 
 void testVitals() {
-    assert(vitalsOk(98.6, 70, 95) == 1);  // Normal case
-    assert(vitalsOk(103, 70, 95) == 0);   // High temperature
-    assert(vitalsOk(98.6, 50, 95) == 0);  // Low pulse rate
-    assert(vitalsOk(98.6, 70, 85) == 0);  // Low oxygen saturation
-    assert(vitalsOk(94, 70, 95) == 0);    // Low temperature
+    ASSERT_EQ(vitalsOk(98.6, 70, 95) == 1);  // Normal case
+    ASSERT_EQ(vitalsOk(103, 70, 95) == 0);   // High temperature
+    ASSERT_EQ(vitalsOk(98.6, 50, 95) == 0);  // Low pulse rate
+    ASSERT_EQ(vitalsOk(98.6, 70, 85) == 0);  // Low oxygen saturation
+    ASSERT_EQ(vitalsOk(94, 70, 95) == 0);    // Low temperature
     cout << "All tests passed!\n";
 }
+
 
