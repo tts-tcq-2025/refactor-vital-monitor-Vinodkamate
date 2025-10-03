511db1afd91144fe21faca467a9baf02589276db
Vinodkamate <vinodkamate4@gmail.com>
2025-10-03 16:47:31 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -11,10 +11,14 @@ bool isPulseRateOk(const float pulseRate);
 bool isSpo2Ok(const float spo2);
 
 // Extension 1: Early Warning System
+bool isInLowWarningRange(const float value, const float min, const float tolerance);
+bool isInHighWarningRange(const float value, const float max, const float tolerance);
 bool isTemperatureWarning(const float temperature);
 bool isPulseRateWarning(const float pulseRate);
 bool isSpo2Warning(const float spo2);
 void displayWarningAlert(const std::string& message);
+void runTestCase(const std::string& testName, const float temp,
+                const float pulse, const float spo2, const bool expected);
 
 // Helper functions
 bool checkVital(const bool isOk, const std::string& alertMessage);
@@ -23,7 +27,7 @@ bool checkVitalWithWarning(const bool isOk, const bool isWarning,
                           const std::string& warningMsg);
 bool checkAllVitals(const float temperature, const float pulseRate,
                     const float spo2);
-bool checkAllVitalsWithWarnings(const float temperature, 
+bool checkAllVitalsWithWarnings(const float temperature,
                                const float pulseRate, const float spo2);
 void displayCriticalAlert(const std::string& message);
 void testVitals();
