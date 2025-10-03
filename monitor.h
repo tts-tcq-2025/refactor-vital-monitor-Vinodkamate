fa142aa1561339dd6ed78cf86c3a149471e093ea
Vinodkamate <vinodkamate4@gmail.com>
2025-10-03 15:28:59 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,14 +1,16 @@
+// Copyright 2025 Health Monitor System
+#ifndef MONITOR_H_
+#define MONITOR_H_
 
-#pragma once
 #include <string>
 int vitalsOk(float temperature, float pulseRate, float spo2);
 bool isInRange(float value, float min, float max);
 bool isTemperatureOk(float temperature);
 bool isPulseRateOk(float pulseRate);
 bool isSpo2Ok(float spo2);
+bool checkVital(bool isOk, const std::string& alertMessage);
 void displayCriticalAlert(const std::string& message);
 void testVitals();
 
-
-
+#endif  // MONITOR_H_
 
