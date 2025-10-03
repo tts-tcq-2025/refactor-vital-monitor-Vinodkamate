5039ef9bd73cc995664d57f2edd7a07d1fa4dbe4
Vinodkamate <vinodkamate4@gmail.com>
2025-10-03 15:18:40 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,10 +1,14 @@
+
 #pragma once
 #include <string>
 int vitalsOk(float temperature, float pulseRate, float spo2);
+bool isInRange(float value, float min, float max);
 bool isTemperatureOk(float temperature);
 bool isPulseRateOk(float pulseRate);
 bool isSpo2Ok(float spo2);
 void displayCriticalAlert(const std::string& message);
 void testVitals();
+
+
 
 
